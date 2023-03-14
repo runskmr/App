15b65c2079fc73b7694e6ee6c3ca7a691fef4e29
runskmr <runskmr14@gmail.com>
2023-03-13 22:02:39 -0400

    Added tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from "@testing-library/react"
+import { Provider } from "react-redux"
+import { MemoryRouter } from "react-router-dom"
+import App from "./App"
+import store from "../src/app/redux"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("App", () => {
+  test("renders App component", () => {
+    const { getByText } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
+    const linkElement = getByText(/Recently Added/i)
+    expect(linkElement).toBeInTheDocument()
+  })
+})
